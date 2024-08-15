62f2f2caabde137984bf10e9c432f45261f9e7fa
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-15 19:50:47 +0300

    feat : Create Course Class

---
+++ EFC01/ITI DB Schema/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFC01.ITI_DB_Schema
+{
+    internal class Course
+    {
+        public int ID { get; set; }
+        public int Duration { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Top_Id { get; set; }
+    }
+}
