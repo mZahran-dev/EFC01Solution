6aae85f787799fdb7a4d53048c77c51114f01f54
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-15 19:54:17 +0300

    feat : Stud_Course Class

---
+++ EFC01/ITI DB Schema/Stud_Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFC01.ITI_DB_Schema
+{
+    internal class Stud_Course
+    {
+        public int stud_ID { get; set; }
+        public int Course_ID { get; set; }
+        public double Grade { get; set; }
+    }
+}
